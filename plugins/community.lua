52216fa7b040e6f6a8ade4046046399bd10ede70
Serge DMYTRIENKO <serge@dmytrienko.fr>
2023-09-17 00:14:20 +0200

    shift

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,4 +6,11 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.colorscheme.gruvbox-baby" },
 }
